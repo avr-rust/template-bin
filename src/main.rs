fc31f32e86da168d7573b2e66dcae6f2d77fdf83
Dylan McKay <me@dylanmckay.io>
2020-07-26 02:13:31 +1200

    Modify the code to allow running tests on the host machine

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 #![no_std]
-#![no_main]
+#![cfg_attr(not(test), no_main)] // #![no_main] interfers with 'cargo test' when targeting the host machine.
 
 extern crate avr_std_stub;
 
 #[no_mangle]
+#[cfg(not(test))] // The main function interfers with 'cargo test' when targeting the host machine.
 fn main() {
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_foo() {
+        assert_eq!(1, 1);
+    }
 }
